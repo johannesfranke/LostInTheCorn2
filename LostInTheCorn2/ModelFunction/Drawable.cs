b52990e497d4c2f4e6a9b247767bfc7227107efe
DIANASLAPTOP\diana <diana-jeske@msn.com>
2024-07-07 12:16:03 +0200

    Erster Attempt Shader (FEHLERHAFT)

--- LostInTheCorn2/ModelFunction/Drawable.cs
+++ LostInTheCorn2/ModelFunction/Drawable.cs
@@ -1,24 +1,37 @@
 ﻿using LostInTheCorn;
+using LostInTheCorn2.Globals;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace LostInTheCorn2.ModelFunction
+namespace LostInTheCorn2.ModelFunction;
+
+public class Drawable
 {
-    public class Drawable
+    public static Effect Effect = Functional.ContentManager.Load<Effect>(@"Effects\Ambient");
+    public static void drawModel(Model model, Matrix pos, Camera cam)
     {
-        public static void drawModel(Model model, Matrix pos, Camera cam)
+        foreach (var mesh in model.Meshes)
         {
-            foreach (var mesh in model.Meshes)
+            //foreach (BasicEffect effect in mesh.Effects)
+            //{
+            //    effect.EnableDefaultLighting();
+            //    effect.View = cam.View;
+            //    effect.World = pos;
+            //    effect.Projection = cam.Projection;
+            //    mesh.Draw();
+            //}
+
+            foreach (ModelMeshPart part in mesh.MeshParts)
             {
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    effect.EnableDefaultLighting();
-                    effect.View = cam.View;
-                    effect.World = pos;
-                    effect.Projection = cam.Projection;
-                    mesh.Draw();
-                }
+                part.Effect = Effect;
+                Effect.Parameters["World"].SetValue(pos);
+                Effect.Parameters["View"].SetValue(cam.View);
+                Effect.Parameters["Projection"].SetValue(cam.Projection);
+                Effect.Parameters["AmbientColor"].SetValue(Color.Pink.ToVector4());
+                Effect.Parameters["AmbientIntensity"].SetValue(0.5f);
             }
+            mesh.Draw();
+
         }
     }
 }
