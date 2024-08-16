c9014e70a842663a2442d5e878799bab5a6bc439
DIANASLAPTOP\diana <diana-jeske@msn.com>
2024-08-16 21:35:45 +0200

    geile Lichteffekte

--- LostInTheCorn2/ModelFunction/Drawable.cs
+++ LostInTheCorn2/ModelFunction/Drawable.cs
@@ -14,23 +14,30 @@ namespace LostInTheCorn2.ModelFunction
                 {
                     effect.EnableDefaultLighting();
                     effect.LightingEnabled = true; // turn on the lighting subsystem.
-                    effect.DirectionalLight0.DiffuseColor = new Vector3(1, 0.7f, 1);
+
+                    effect.DirectionalLight0.DiffuseColor = new Vector3(0.9f, 0.6f, 0.4f);
                     effect.DirectionalLight0.Direction = new Vector3(0, -0.2f, -2);
-                    effect.DirectionalLight0.SpecularColor = new Vector3(0.6f, 0.3f, 0.5f); //r,g,b
+                    effect.DirectionalLight0.SpecularColor = new Vector3(0, 0, 0); //r,g,b
 
-                    //effect.DirectionalLight1.DiffuseColor = new Vector3(1, 1, 1); //
-                    //effect.DirectionalLight1.Direction = new Vector3(4, 0, 0);  // coming along the x-axis
-                    //effect.DirectionalLight1.SpecularColor = new Vector3(1, 1, 1); // with green highlights
+                    effect.DirectionalLight1.DiffuseColor = new Vector3(0, 0, 0);
+                    effect.DirectionalLight1.Direction = new Vector3(-2, -0.2f, -2);
+                    effect.DirectionalLight1.SpecularColor = new Vector3(0.9f, 0.4f, 0.3f);
 
-                    //effect.DirectionalLight2.DiffuseColor = new Vector3(1, 1, 1); //
-                    //effect.DirectionalLight2.Direction = new Vector3(-1, 0, 0);  // coming along the x-axis
-                    //effect.DirectionalLight2.SpecularColor = new Vector3(1, 1, 1); // with green highlights
+                    effect.DirectionalLight2.DiffuseColor = new Vector3(0.4f, 0.05f, 0.5f);
+                    effect.DirectionalLight2.Direction = new Vector3(0, -0.2f, -2);
+                    effect.DirectionalLight2.SpecularColor = new Vector3(0, 0, 0);
 
-                    effect.AmbientLightColor = new Vector3(0.7f, 0.4f, 0.2f);
-                    effect.EmissiveColor = new Vector3(0.5f, 0.2f, 0.2f);
+                    effect.AmbientLightColor = new Vector3(0.8f, 0.3f, 0.6f);
+                    effect.EmissiveColor = new Vector3(0, 0, 0.2f);
                     effect.View = cam.View;
                     effect.World = pos;
                     effect.Projection = cam.Projection;
+
+                    effect.FogEnabled = true;
+                    effect.FogStart = 100f;
+                    effect.FogEnd = 1000f;
+                    effect.FogColor = new Vector3(0.01f, 0.2f, 0.12f); //dunkle Grün
+
                     mesh.Draw();
                 }
             }
